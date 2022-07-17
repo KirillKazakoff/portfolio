f08c01cc3977d596ac711771f6cd1b0db94160c9
keka3avr <kekazavr228@gmail.com>
2022-07-17 23:18:40 +1100

    layout has been conveyed

---
+++ webpack.config.js
@@ -0,0 +1,58 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+    entry: './src/index.tsx',
+    resolve: {
+        extensions: ['.ts', '.tsx', '.js'],
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(s(a|c)ss)$/,
+                use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+            },
+            {
+                test: /\.(ts|tsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                },
+            },
+            {
+                test: /\.(png|jpg|gif|ttf|ico|svg)$/i,
+                type: 'asset/resource',
+            },
+        ],
+    },
+    devServer: {
+        client: {
+            logging: 'none',
+        },
+        port: 9000,
+        compress: true,
+        historyApiFallback: true,
+        https: false,
+        hot: true,
+    },
+    devtool: 'source-map',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+        // publicPath: '',
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.join(__dirname, 'src/index.html'),
+        }),
+        new MiniCssExtractPlugin(),
+        new CopyPlugin({
+            patterns: [
+                { from: 'src/img', to: 'img' },
+                { from: 'src/svg', to: 'svg' },
+            ],
+        }),
+    ],
+};
